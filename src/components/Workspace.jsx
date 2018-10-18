501509c182307d3ff22ed4eb7569f3edd6eadd84
Mandy Kong <mk5506@nyu.edu>
2018-10-18 07:55:28 -0400

    creating Workspace component

---
+++ src/components/Workspace.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+class Workspace extends React.Component {
+  render () {
+    return (
+     	<div id="workspace">
+     		<div className="sidebar"/>
+     	</div>
+    )
+  }
+}
+
+export default Workspace
