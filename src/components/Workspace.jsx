78e479bf269b1bb0775462819a4a967f9f12b570
Mandy Kong <mk5506@nyu.edu>
2018-10-27 21:24:59 -0400

    Workspace now has sidebar

--- src/components/Workspace.jsx
+++ src/components/Workspace.jsx
@@ -1,13 +1,69 @@
 import React from 'react'
+import { connect } from 'react-redux'
+import { Menu, Icon, Header, Divider } from 'semantic-ui-react'
+
+import '../css/Workspace.css'
 
 class Workspace extends React.Component {
+  constructor (props) {
+    super(props)
+
+    this.state = {
+      activeItem: 'workspace'
+    }
+  }
+
+  componentDidMount () {
+    const endpoint = `/${this.props.userId}/${this.state.activeItem}`
+  }
+
+  handleItemClick = (event, name) => {
+    this.setState({ activeItem: name })
+  }
+
+  renderMenuList = (items) => {
+    return items.map((item, i) => {
+      return (
+        <Menu.Item
+          name={item.name}
+          key={i}
+          active={(this.state.activeItem === item.name)}
+          onClick={(event) => { this.handleItemClick(event, item.name) }}
+        >
+          <Icon name={item.icon} />
+          { item.title }
+        </Menu.Item>
+      )
+    })
+  }
+
   render () {
+    const items = [
+      { "title": 'My Workspace', "name": 'workspace', "icon": 'write' },
+      { "title": 'Shared with me', "name": 'shared', "icon": 'users' },
+      { "title": 'Recent', "name": 'recent', "icon": 'clock' },
+      { "title": 'Starred', "name": 'starred', "icon": 'star' },
+      { "title": 'Trash', "name": 'trash', "icon": 'trash alternate' }
+    ]
+
     return (
-     	<div id="workspace">
-     		<div className="sidebar"/>
-     	</div>
+      <div>
+        <Menu icon="labeled" vertical pointing fixed="left">
+          <Menu.Item header> 
+            <Header as="h2"> MangoNotes </Header>
+            <Divider hidden />
+          </Menu.Item>
+          { this.renderMenuList(items) }
+        </Menu>
+      </div>
     )
   }
 }
 
-export default Workspace
+function mapStateToProps (state) {
+  return {
+    userId: state.user.userId
+  }
+}
+
+export default connect(mapStateToProps)(Workspace)
