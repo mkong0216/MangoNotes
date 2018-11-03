c7f23ce2ff80c26b351101dbb196ab49770424a1
Mandy Kong <mk5506@nyu.edu>
2018-11-03 19:52:56 -0400

    render notebooks of current folder

--- src/components/Workspace.jsx
+++ src/components/Workspace.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { connect } from 'react-redux'
 import PropTypes from 'prop-types'
 
 import { Card } from 'semantic-ui-react'
@@ -6,11 +7,13 @@ import CreateModal from './CreateModal'
 
 import '../css/Workspace.css'
 import plus from '../images/plus-icon.png'
+import notebookIcon from '../images/notebook.png'
 
 class Workspace extends React.Component {
   static propTypes = {
     updateCurrentPath: PropTypes.func.isRequired,
-    currPath: PropTypes.array.isRequired
+    currPath: PropTypes.array.isRequired,
+    notebooks: PropTypes.array
   }
 
   constructor (props) {
@@ -25,10 +28,35 @@ class Workspace extends React.Component {
     this.setState({ showModal: !this.state.showModal })
   }
 
+  renderNotebooks = (notebooks) => {
+    return notebooks.map((notebook, i) => {
+      const { timestamp } = notebook
+      const month = timestamp.getMonth() + 1
+      const day = timestamp.getDate()
+      const year = timestamp.getFullYear()
+
+      const modifiedOn = `Last modified on ${month}/${day}/${year}`
+
+      return (
+        <Card
+          key={i}
+          image={notebookIcon}
+          header={notebook.title}
+          meta={modifiedOn}
+        />
+      )
+    })
+  }
+
   render () {
+    const { currPath, notebooks } = this.props
+    const currNotebook = currPath[currPath.length - 1]
+    const notebooksToRender = notebooks.filter(notebook => notebook.parentNotebook === currNotebook)
+
     return (
       <React.Fragment>
         <Card.Group id="workspace" itemsPerRow={6}>
+          { this.renderNotebooks(notebooksToRender) }
           <Card color="olive" image={plus} onClick={this.toggleModal} />
         </Card.Group>
         <CreateModal
@@ -42,4 +70,10 @@ class Workspace extends React.Component {
   }
 }
 
-export default Workspace
+function mapStateToProps (state) {
+  return {
+    notebooks: state.notebooks.userNotebooks
+  }
+}
+
+export default connect(mapStateToProps)(Workspace)
