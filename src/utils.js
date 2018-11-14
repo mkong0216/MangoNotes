5564d6e58e815b6b2dc92d09bad38b48e03e1987
Mandy Kong <mk5506@nyu.edu>
2018-11-14 11:44:07 -0500

    if date from mongodb, date is a string so create date object

--- src/utils.js
+++ src/utils.js
@@ -1,5 +1,13 @@
-// Given a Date object, returns the string month/day/year
+/**
+ * Given a Date object or string, display date in month/day/year format.
+ * 
+ * @param {Object or String} timestamp 
+ */
 export function getDateModified (timestamp) {
+    if (typeof timestamp === 'string') {
+        timestamp = new Date(timestamp)
+    }
+
     const month = timestamp.getMonth() + 1
     const day = timestamp.getDate()
     const year = timestamp.getFullYear()
