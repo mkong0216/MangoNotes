359a85fd2dab83eb4e888dd76459d98e771c2cbe
Mandy Kong <mk5506@nyu.edu>
2018-11-20 19:43:50 -0500

    added path to Dashboard when logged in

--- src/components/App.js
+++ src/components/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { Switch, Route } from 'react-router-dom'
 
 import SignInMenu from './SignInMenu'
+import Dashboard from './Dashboard'
 
 class App extends Component {
   render() {
@@ -9,6 +10,7 @@ class App extends Component {
       <div className="App">
         <Switch>
           <Route exact path="/" component={SignInMenu} />
+          <Route path="/:username" component={Dashboard} />
         </Switch>
       </div>
     );
