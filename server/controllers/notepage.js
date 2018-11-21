b3f4fa1aea3558a5bf1b26a90297731a2cb6e754
Mandy Kong <mk5506@nyu.edu>
2018-11-21 13:47:08 -0500

    updated CreateNotepage function

--- server/controllers/notepage.js
+++ server/controllers/notepage.js
@@ -1,25 +1,50 @@
 const Notepage = require('../models/Notepage')
+const Notebook = require('../models/Notebook')
 
 exports.CreateNotepage = function (req, res) {
-  const username = req.params.username
+  const userId = req.body.creator
+  let details = { ...req.body, type: 'notepage' }
+
+  if (!userId) {
+    res.status(401).send("Failed to provide a userId")
+  }
+
+  const handleUpdateNotebook = function (error, parentNotebook) {
+    if (error) {
+      console.log(error)
+      res.status(500).send("Error finding and updating parent notebook in database")
+    } else {
+      res.status(200).json(details)
+    }
+  }
 
   const handleSaveNotepage = function (error, notepage) {
     if (error) {
       console.log(error)
       res.status(500).send("Error creating new notepage in database.")
     } else {
-      res.status(200).json({
-        id: notepage._id,
-        title: notepage.title,
-        timestamp: notepage.timestamp
-      })
+      details.id = notepage._id
+      if (notepage.parentNotebook) {
+        Notebook.findOneAndUpdate({ title: notepage.parentNotebook, creator: userId }, { $push: { content: details }}, handleUpdateNotebook)
+      } else {
+        res.status(200).json(details)
+      }
     }
   }
 
-  if (username) {
-    const newNotepage = new Notepage(req.body)
-    newNotepage.save(handleSaveNotepage)
-  } else {
-    res.status(400).send("Notepage does not have a creator")
+  const handleCheckDuplicates = function (error, count) {
+    if (error) {
+      console.log(error)
+      res.status(500).send("Error checking for duplicate notepage")
+    } else if (count > 0) {
+      res.status(401).json({
+        error: "There already exists a notepage with this name. Please rename the notepage."
+      })
+    } else {
+      const newNotepage = new Notepage(req.body)
+      newNotepage.save(handleSaveNotepage)
+    }
   }
+
+  Notepage.countDocuments(req.body, handleCheckDuplicates)
 }
