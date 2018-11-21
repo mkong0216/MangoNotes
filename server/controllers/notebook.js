087f8926bc898d6c8cb053bb82a2e958e45533a2
Mandy Kong <mk5506@nyu.edu>
2018-11-21 12:20:19 -0500

    added handling get notebook requests; updated createnotebook to check for duplicates and update parentNotebook contents

--- server/controllers/notebook.js
+++ server/controllers/notebook.js
@@ -2,9 +2,19 @@ const Notebook = require('../models/Notebook')
 
 exports.CreateNotebook = function (req, res) {
   const userId = req.body.creator
+  let details = { ...req.body, type: 'notebook' }
 
   if (!userId) {
     res.status(401).send("Failed to provide a userId")
+  }
+
+  const handleUpdateNotebook = function (error, parentNotebook) {
+    if (error) {
+      console.log(error)
+      res.status(500).send("Error finding and updating parent notebook in database")
+    } else {
+      res.status(200).json(details)
+    }
   }
 
   const handleSaveNotebook = function (error, notebook) {
@@ -12,11 +22,12 @@ exports.CreateNotebook = function (req, res) {
       console.log(error)
       res.status(500).send("Error creating new notebook in database.")
     } else {
-      res.status(200).json({
-        id: notebook._id,
-        title: notebook.title,
-        creator: notebook.creator
-      })
+      details.id = notebook._id
+      if (notebook.parentNotebook) {
+        Notebook.findOneAndUpdate({ title: notebook.parentNotebook, creator: userId }, { $push: { content: details }}, handleUpdateNotebook)
+      } else {
+        res.status(200).json(details)
+      }
     }
   }
 
@@ -34,5 +45,31 @@ exports.CreateNotebook = function (req, res) {
     }
   }
 
-  Notebook.estimatedDocumentCount(req.body, handleCheckDuplicates)
+  Notebook.countDocuments(req.body, handleCheckDuplicates)
+}
+
+exports.GetNotebook = function (req, res) {
+  const notebookId = req.params.notebookId
+  const userId = req.params.userId
+
+  if (!notebookId) {
+    res.status(401).send("Failed to provide a notebook id")
+  }
+
+  const handleFindNotebook = function (error, notebook) {
+    if (error) {
+      console.log(error)
+      res.status(500).send("Error finding notebook in database")
+    } else {
+      if (userId && notebook.creator === userId) {
+        res.status(200).send(notebook.content)
+      } else {
+        res.status(401).json({
+          error: "Looks like you don't have the right permissions to view the notebook."
+        })
+      }
+    }
+  }
+
+  Notebook.findById(notebookId, handleFindNotebook)
 }
