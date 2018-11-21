921cf882d51a9bf32e9bfb5c553077d4da9b7251
Mandy Kong <mk5506@nyu.edu>
2018-11-20 23:48:51 -0500

    updated comments to reflect addition of creator; pass response.data not response to updateUsersWork function

--- src/xhr/notebook.js
+++ src/xhr/notebook.js
@@ -4,14 +4,15 @@ import { updateUsersWork } from './user'
 /**
  * Makes a POST request to DB to create new Notebook object
  * 
- * @param {Object} notebook - in shape of { title, userId, parentNotebook }
+ * @param {Object} notebook - in shape of { title, creator, parentNotebook }
  * 
  */
 export async function createNewNotebook (notebook) {
   const endpoint = '/notebook/new'
 
   try {
-    const details = await axios.post(endpoint, notebook)
+    const response = await axios.post(endpoint, notebook)
+    const details = response.data
 
     if (!notebook.parentNotebook) {
       updateUsersWork(notebook.creator, details, 'notebook')
