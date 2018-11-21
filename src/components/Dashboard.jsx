c69a520c3167cf4ee8ef21a0a4c048c8afe3a5f9
Mandy Kong <mk5506@nyu.edu>
2018-11-21 12:21:30 -0500

    attempting to figure out how to retrieve current items from current parent notebook

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -6,6 +6,7 @@ import { Redirect } from 'react-router-dom'
 import { Grid } from 'semantic-ui-react'
 import SidebarMenu from './SidebarMenu'
 import NoteCards from './NoteCards'
+import { retrieveNotebook } from '../xhr/notebook'
 
 /**
  * Dashboard.jsx
@@ -17,32 +18,57 @@ class Dashboard extends React.Component {
   static propTypes = {
     notebooks: PropTypes.array,
     notepages: PropTypes.array,
-    signedIn: PropTypes.bool.isRequired
+    user: PropTypes.object.isRequired
   }
 
   constructor (props) {
     super(props)
 
     this.state = {
-      currentPath: []
+      currentPath: ['workspace'],
+      contents: null,
+      parentNotebook: null
     }
   }
 
-  updateCurrentPath = (path) => { this.setState({ currentPath: path }) }
+  updateCurrentPath = (path, isMenuItem) => {
+    const currentPath = (isMenuItem) ? path : this.state.currentPath.concat(path)
+    const parentNotebook = (currentPath.length > 1) ? currentPath[currentPath.length - 1] : null
+    this.setState({ currentPath, parentNotebook })
+
+    this.getNotebooksAndNotepages(parentNotebook)
+  }
+
+  getNotebooksAndNotepages = async (parentNotebook) => {
+    const historyState = window.history.state
+
+    let noteItems = {}
+
+    if (!parentNotebook && this.state.currentPath[0] === 'workspace') {
+      noteItems = {
+        notebooks: this.props.notebooks,
+        notepages: this.props.notepages
+      }
+    } else if (parentNotebook && historyState.type === 'notebook') {
+      noteItems = await retrieveNotebook(historyState.noteId, this.props.user.signInData.userId)
+    }
+
+    console.log(noteItems)
+    this.setState({ contents: noteItems })
+  }
 
   render () {
-    const { currentPath } = this.state
-    const parentNotebook = (currentPath.length > 1) ? currentPath[currentPath.length - 1] : null
+    const { parentNotebook, contents } = this.state
 
-    return this.props.signedIn ? (
+    return this.props.user.signedIn ? (
       <Grid className="dashboard">
         <Grid.Column width={3}>
           <SidebarMenu updateCurrentPath={this.updateCurrentPath} />
         </Grid.Column>
         <Grid.Column width={13}>
           <NoteCards
-            notebooks={this.props.notebooks}
-            notepages={this.props.notepages}
+            notebooks={contents && contents.notebooks}
+            notepages={contents && contents.notepages}
             parentNotebook={parentNotebook}
             updateCurrentPath={this.updateCurrentPath}
           />
@@ -58,7 +84,7 @@ function mapStateToProps (state) {
   return {
     notebooks: state.notebooks.userNotebooks,
     notepages: state.notepages.userNotepages,
-    signedIn: state.user.signedIn
+    user: state.user
   }
 }
 
