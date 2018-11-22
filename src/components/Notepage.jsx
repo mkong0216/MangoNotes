67f4b3d08f46a405e89e7c32c46ed95ca6cdcf40
Mandy Kong <mk5506@nyu.edu>
2018-11-22 10:46:56 -0500

    renamed TextArea to NotepageText

--- src/components/Notepage.jsx
+++ src/components/Notepage.jsx
@@ -5,7 +5,7 @@ import { Redirect } from 'react-router-dom'
 
 import { Grid, Header, Image, Popup, Input } from 'semantic-ui-react'
 import Toolbar from './Toolbar'
-import TextArea from './TextArea'
+import NotepageText from './NotepageText'
 
 import { retrieveNotepage, updateNotepage } from '../xhr/notepage'
 import notepageIcon from '../images/notepage.png'
@@ -110,7 +110,7 @@ class Notepage extends React.Component {
               <Toolbar />
             </Grid.Column>
             <Grid.Column width={10}>
-              <TextArea />
+              <NotepageText />
             </Grid.Column>
             <Grid.Column width={3}>
               Search Bar
