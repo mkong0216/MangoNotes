7b25ea687050da547010f563d7222f40ece77379
Mandy Kong <mk5506@nyu.edu>
2018-11-21 19:43:20 -0500

    added functions to handle retrieving and updating notepages

--- src/xhr/notepage.js
+++ src/xhr/notepage.js
@@ -6,7 +6,7 @@ import { updateUsersWork } from './user'
  *
  * @param {Object} notepage - in shape of { title, creator, parentNotebook }
  *
- * @returns {Object} - in shape of { creator, notebookId, parentNotebook, title, type }
+ * @returns {Object} - in shape of { creator, notebookId, parentNotebook, title, type, updatedAt }
  */
 export async function createNewNotepage (notepage) {
   const endpoint = '/notepage/new'
@@ -16,11 +16,60 @@ export async function createNewNotepage (notepage) {
     const details = response.data
 
     if (!notepage.parentNotebook) {
-      updateUsersWork(notepage.creator, details)
+      await updateUsersWork(notepage.creator, details)
     }
 
     return details
   } catch (error) {
+    throw Error (error.response.data.error)
+  }
+}
+
+/**
+ * Gets notepage from database
+ *
+ * @param {String} notepageId
+ * @param {String} userId
+ *
+ * @returns {Object} - in shape of { parentNotebook, title, updatedAt, content }
+ */
+export async function retrieveNotepage (notepageId, userId) {
+  const endpoint = `/notepage/${notepageId}/${userId}`
+
+  try {
+    const response = await axios.get(endpoint)
+    const notepage = {
+      title: response.data.title,
+      parentNotebook: response.data.parentNotebook,
+      updatedAt: response.data.updatedAt,
+      content: response.data.content || ''
+    }
+
+    return notepage
+  } catch (error) {
+    throw Error(error.response.data.error)
+  }
+}
+
+/**
+ * Updates notepage in database
+ *
+ * @param {Object} notepage - in shape of { title, parentNotebook, content, notepageId }
+ *
+ */
+export async function updateNotepage(notepage, userId) {
+  const endpoint = `/notepage/${notepage.notepageId}/${userId}`
+
+  try {
+    const response = await axios.put(endpoint, notepage)
+
+    if (!notepage.parentNotebook) {
+      updateUsersWork(userId, response.data)
+    }
+
+    return response.data
+  } catch (error) {
+    console.log(error)
     throw Error (error.response.data.error)
   }
 }
