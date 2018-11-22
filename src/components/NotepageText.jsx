c1996ddb262cc4c942425a0f1b0e2cf74fb5c6a2
Mandy Kong <mk5506@nyu.edu>
2018-11-22 10:47:27 -0500

    renamed TextArea to NotepageText

---
+++ src/components/NotepageText.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { TextArea } from 'semantic-ui-react'
+import '../css/NotepageText.css'
+
+class NotepageText extends React.Component {
+  render () {
+    return (
+      <TextArea autoHeight />
+    )
+  }
+}
+
+export default NotepageText
