1509e28078dfc67aaef14c70d9a0ac1963ec263d
Mandy Kong <mk5506@nyu.edu>
2018-12-03 22:36:15 -0500

    renamed options

--- server/models/Settings.js
+++ server/models/Settings.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose')
 
 let SettingSchema = new mongoose.Schema({
     username: { type: String, required: true },
-    option: { type: String, required: true }
+    options: { type: String, required: true }
 })
 
 module.exports = mongoose.model('Settings', SettingSchema)
