35b7878f62c74daf31fff6db4f26b7ecf1d70ca1
Mandy Kong <mk5506@nyu.edu>
2018-12-04 16:52:29 -0500

    added constants to note item types; created function to generate permission code

--- src/utils.js
+++ src/utils.js
@@ -1,3 +1,5 @@
+import shortid from 'shortid'
+
 export function getElAbsolutePos (el, includeScroll = false) {
     let pos = [0, 0]
   
@@ -16,4 +18,13 @@ export function getElAbsolutePos (el, includeScroll = false) {
     } while (el)
   
     return pos
+}
+
+export const TYPE_NOTEBOOK = 'notebook'
+export const TYPE_NOTEPAGE = 'notepage'
+
+export function createPermissionCode (readOnly) {
+  const code = shortid.generate()
+  const extra = (readOnly) ? 0 : 1
+  return code + extra
 }
